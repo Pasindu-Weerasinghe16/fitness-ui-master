a9cf0032a1784f79a727a4341724851cdcc1f113
Pasindu Weerasinghe <eg245398@engug.ruh.ac.lk>
2026-01-19 10:19:45 +0530

    Revert "article pages"

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -20,11 +20,4 @@ class AppRoutes {
 
   // Insights & charts
   static const String insights = '/insights';
-
-  // Article topics
-  static const String nutritionArticles = '/articles/nutrition';
-  static const String trainingArticles = '/articles/training';
-  static const String recoveryArticles = '/articles/recovery';
-  static const String motivationArticles = '/articles/motivation';
-  static const String wellnessArticles = '/articles/wellness';
 }
